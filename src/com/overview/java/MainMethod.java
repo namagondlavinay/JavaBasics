0031d3f48f2f4cc17e2cc83e51eb04e9d51334fa
vinay kumar <namagondlavinay@gmail.com>
2019-04-27 20:17:19 +0530

    my first commit

---
+++ src/com/overview/java/MainMethod.java
@@ -0,0 +1,19 @@
+package com.overview.java;
+
+public class MainMethod {
+
+	public static void main(String[] args) {
+/* 
+		public: So that JVM can execute the method from anywhere.
+		static: Main method is to be called without object. 
+		The modifiers public and static can be written in either order.
+		void: The main method doesn't return anything.
+		main(): Name configured in the JVM.
+		String[]: The main method accepts a single argument: 
+		          an array of elements of type String.
+*/
+		System.out.println("Hello World");
+		
+	}
+
+}
